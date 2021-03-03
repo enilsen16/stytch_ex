b6310af37d6136fd8ff4ce6f8a8af97162565125
Erik Nilsen <enilsen16@live.com>
2021-03-03 17:21:53 +0800

    No longer use instance variables

--- lib/stytch_ex/helpers.ex
+++ lib/stytch_ex/helpers.ex
@@ -1,8 +1,28 @@
 defmodule StytchEx.Helpers do
-  @username Application.get_env(:stytch_ex, :username, "")
-  @password Application.get_env(:stytch_ex, :password, "")
+  def base_url(), do: Application.get_env(:stytch_ex, :base_url, "https://test.stytch.com/v1/")
 
-  def build_auth_token() do
-    Base.encode64("#{@username}:#{@password}")
+  def magic_link_url() do
+    Application.get_env(
+      :stytch_ex,
+      :magic_link_url,
+      "https://example.com/authenticate"
+    )
   end
+
+  def headers() do
+    [
+      {"Content-Type", "application/json"},
+      {"Authorization", "Basic #{build_auth_token()}"}
+    ]
+  end
+
+  defp username() do
+    Application.get_env(:stytch_ex, :username, "")
+  end
+
+  defp password() do
+    Application.get_env(:stytch_ex, :password, "")
+  end
+
+  defp build_auth_token(), do: Base.encode64("#{username()}:#{password()}")
 end
--- lib/stytch_ex/magic_links.ex
+++ lib/stytch_ex/magic_links.ex
@@ -4,46 +4,35 @@ defmodule StytchEx.MagicLinks do
   """
   alias StytchEx.Helpers
 
-  @base_url Application.get_env(:stytch_ex, :base_url, "https://test.stytch.com/v1/")
-  @magic_link_url Application.get_env(
-                    :stytch_ex,
-                    :magic_link_url,
-                    "https://example.com/authenticate"
-                  )
-  @headers [
-    {"Content-Type", "application/json"},
-    {"Authorization", "Basic #{Helpers.build_auth_token()}"}
-  ]
-
   def send_by_email(email) do
-    url = @base_url <> "magic_links/send_by_email"
+    url = Helpers.base_url() <> "magic_links/send_by_email"
 
     body = build_body(email)
 
-    Finch.build(:post, url, @headers, body)
+    Finch.build(:post, url, Helpers.headers(), body)
     |> Finch.request(MyFinch)
   end
 
   def login_or_create(email) do
-    url = @base_url <> "magic_links/login_or_create"
+    url = Helpers.base_url() <> "magic_links/login_or_create"
     body = build_body(email)
 
-    Finch.build(:post, url, @headers, body)
+    Finch.build(:post, url, Helpers.headers(), body)
     |> Finch.request(MyFinch)
   end
 
   def authenticate(token) do
-    url = @base_url <> "magic_links/#{token}/authenticate"
+    url = Helpers.base_url() <> "magic_links/#{token}/authenticate"
 
-    Finch.build(:post, url, @headers)
+    Finch.build(:post, url, Helpers.headers())
     |> Finch.request(MyFinch)
   end
 
   defp build_body(email) do
     data = %{
       email: email,
-      login_magic_link_url: @magic_link_url,
-      signup_magic_link_url: @magic_link_url
+      login_magic_link_url: Helpers.magic_link_url(),
+      signup_magic_link_url: Helpers.magic_link_url()
     }
 
     Jason.encode!(data)
